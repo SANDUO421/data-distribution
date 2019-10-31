467357dc0c09df7f56004414a066c9d3d907423b
Pano <362472407@qq.com>
2019-10-31 09:07:21 +0800

    端口修改

--- data-distribution-server/src/main/java/com/data/distribution/service/impl/DataGetServiceImpl.java
+++ data-distribution-server/src/main/java/com/data/distribution/service/impl/DataGetServiceImpl.java
@@ -68,7 +68,7 @@ public class DataGetServiceImpl implements DataGetService {
                     if (socket == null || socket.isClosed()) {
                         // 连接socket
 //                        socket = new Socket("192.168.1.213", 9999);
-                        socket = new Socket("192.168.20.153", 8888);
+                        socket = new Socket("192.168.100.4", 2005);
                         inputStream = new DataInputStream(socket.getInputStream());
                     }
                     String result = inputStream.readUTF();
